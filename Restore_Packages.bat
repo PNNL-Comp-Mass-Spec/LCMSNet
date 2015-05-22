1d1e0acde4485df41c558e50800957ed92899ce8
alchemistmatt <matthew.monroe@pnnl.gov>
2015-05-22 15:10:55 -0700

    Add NUnit via NuGet for LcmsNetSQLiteToolsUnitTests Remove Restore_Packages.bat

--- Restore_Packages.bat
+++
@@ -1,6 +0,0 @@
-rem This batch file is used by Jenkins to restore packages prior to building the solutions
-nuget.exe restore LcmsNet\SDK\LcmsNetSDKs.sln
-nuget.exe restore LcmsNet\SQLiteTools\SQLiteTools.sln
-nuget.exe restore LcmsNetPlugins\lcmsnetPlugins.sln
-nuget.exe restore LcmsNetDmsTools\LcmsNetDmsTools.sln
-nuget.exe restore LcmsNet\lcms\LCMSnet\LCMSNet.sln
