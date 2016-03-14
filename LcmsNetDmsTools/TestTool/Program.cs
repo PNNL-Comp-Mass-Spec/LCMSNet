5780acc70ade3e2a59c95d947b4ea7a194e7a1ff
alchemistmatt <matthew.monroe@pnnl.gov>
2016-03-14 15:51:11 -0700

    Show messages and exceptions from classDBTools at the console by subscribing to events from classApplicationLogger

--- LcmsNetDmsTools/TestTool/Program.cs
+++ LcmsNetDmsTools/TestTool/Program.cs
@@ -1,4 +1,6 @@
-﻿using LcmsNetDmsTools;
+﻿using System;
+using LcmsNetDataClasses.Logging;
+using LcmsNetDmsTools;
 
 namespace TestTool
 {
@@ -6,15 +8,44 @@ namespace TestTool
     {
         static void Main(string[] args)
         {
-            var dbt = new classDBTools
+            try
             {
-                LoadExperiments = true,
-                LoadDatasets = true,
-                RecentExperimentsMonthsToLoad = 18,
-                RecentDatasetsMonthsToLoad = 12
-            };
+                classApplicationLogger.Error += classApplicationLogger_Error;
+                classApplicationLogger.ErrorLevel = 3;
 
-            dbt.LoadCacheFromDMS();
+                classApplicationLogger.Message += classApplicationLogger_Message;
+                classApplicationLogger.MessageLevel = 2;
+
+                var dbt = new classDBTools
+                {
+                    LoadExperiments = true,
+                    LoadDatasets = true,
+                    RecentExperimentsMonthsToLoad = 18,
+                    RecentDatasetsMonthsToLoad = 12
+                };
+
+                dbt.LoadCacheFromDMS();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(@"Exception: " + ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        static void classApplicationLogger_Error(int errorLevel, classErrorLoggerArgs args)
+        {
+            Console.WriteLine(@"=== Exception ===");
+            Console.WriteLine(args.Message);
+
+            if (!string.Equals(args.Message, args.Exception.Message))
+                Console.WriteLine(args.Exception.Message);
+        }
+
+        static void classApplicationLogger_Message(int messageLevel, classMessageLoggerArgs args)
+        {
+            Console.WriteLine(args.Message);
         }
     }
 }
