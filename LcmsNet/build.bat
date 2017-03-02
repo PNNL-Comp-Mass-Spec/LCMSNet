33034c80b5f20b82fd78367c53a4ee894c07fce7
alchemistmatt <matthew.monroe@pnnl.gov>
2017-03-02 12:02:37 -0800

    Update build .bat files

--- LcmsNet/build.bat
+++ LcmsNet/build.bat
@@ -1,52 +1,71 @@
 @echo off
-REM Build.bat 
-REM Batch file for building lcmsnet with dmstools and plugins as if using the PNNL internal installer.
-REM Written by Christopher Walters 10/22/2014
-REM Last Modified 10/22/2014
-REM Path to MSBuild must be in your PATH environment variable in order for this batch file to work.
+rem Build.bat 
+rem Batch file for building lcmsnet with dmstools and plugins as if using the PNNL internal installer.
+rem Written by Christopher Walters 10/22/2014
+rem Updated 03/02/2017 to auto-define the path to MSBuild.exe if necessary
 
 SETLOCAL
 SET ROOTPATH=%~dp0
 SET CONFIG=/p:Configuration=PNNLRelease /p:Platform=x86
 SET VERB=/verbosity:quiet
 SET LOG=/fileLogger
-SET LOGFILE=%ROOTPATH%lcmsnetBuild.txt
+SET LOGFILE="%ROOTPATH%lcmsnetBuild.txt"
 SET LGPARAMS=/fileloggerparameters:logfile=%LOGFILE%;append=true
-REM SET NOLOGO=/nologo
+rem SET NOLOGO=/nologo
 SET BOPTS=%VERB% %LOG% %LGPARAMS% 
 
+rem Determine the path to MSBuild
+where /q msbuild.exe
+IF ERRORLEVEL 1 (
+   echo "Using MSBuild.exe at C:\Program Files (x86)\MSBuild\14.0\Bin\"
+   echo.
+   SET MSBuildExe="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
+) Else (
+   echo "Using MSBuild.exe in your path"
+   echo.
+   Set MSBuildExe=msbuild.exe
+)
+
 if exist %LOGFILE% DEL %LOGFILE%
-REM Build LcmsNetSDK solution
-ECHO Building LcmsNetSDK solution...
-MSBuild %CONFIG% %BOPTS% %ROOTPATH%SDK\LcmsNetSDKs.sln
-if errorlevel 1 goto :ERROR
-ECHO Done.
 
-REM Build LcmsNetSQLiteTools solution
-ECHO Building LcmsNetSQLiteTools solution...
-MSBUILD %CONFIG% %BOPTS% %ROOTPATH%SQLiteTools\SQLiteTools.sln
+rem Build LcmsNetSDK solution
+echo Building LcmsNetSDK solution...
+@echo on
+%MSBuildExe% %CONFIG% %BOPTS% "%ROOTPATH%SDK\LcmsNetSDKs.sln"
+@echo off
 if errorlevel 1 goto :ERROR
-ECHO Done.
+echo Done.
 
-REM Build Main solution.
-ECHO Building LcmsNet...
-MSBUILD /p:Configuration=Debug /p:targetPlatform=x86 %BOPTS% %ROOTPATH%lcms\LCMSNet\LCMSNet.sln
+rem Build LcmsNetSQLiteTools solution
+echo Building LcmsNetSQLiteTools solution...
+@echo on
+%MSBuildExe% %CONFIG% %BOPTS% "%ROOTPATH%SQLiteTools\SQLiteTools.sln"
+@echo off
 if errorlevel 1 goto :ERROR
-ECHO Done.
+echo Done.
 
-ECHO Copying Core Validator to %APPDATA%\LCMSNet\SampleValidators
-xcopy /y /q %ROOTPATH%SDK\CoreSampleValidator\bin\x86\PNNLRelease\*.dll %APPDATA%\LCMSNet\SampleValidators\
-ECHO Done. 
+rem Build Main solution.
+echo Building LcmsNet...
+@echo on
+%MSBuildExe% /p:Configuration=Debug /p:targetPlatform=x86 %BOPTS% "%ROOTPATH%lcms\LCMSNet\LCMSNet.sln"
+@echo off
+if errorlevel 1 goto :ERROR
+echo Done.
+
+echo Copying Core Validator to %APPDATA%\LCMSNet\SampleValidators
+xcopy /y /q /d "%ROOTPATH%SDK\CoreSampleValidator\bin\x86\PNNLRelease\*.dll" %APPDATA%\LCMSNet\SampleValidators\
+echo Done. 
 if errorlevel 1 goto :ERROR
 goto :COMPLETE
 
 :ERROR
-ECHO Error occured during build. See lcmsnetBuild.txt for more details.
+echo Error occured during build. See lcmsnetBuild.txt for more details.
 goto :END
 
 :COMPLETE
-ECHO Cleaning up...
+echo Cleaning up...
 DEL %LOGFILE%
-ECHO BUILD COMPLETE.
+
+echo BUILD COMPLETE.
 
 :END
--- buildall.bat
+++ buildall.bat
@@ -1,95 +1,123 @@
 @echo off
-REM Build.bat 
-REM Batch file for building lcmsnet with dmstools and plugins as if using the PNNL internal installer.
-REM Written by Christopher Walters 9/12/2014
-REM Last Modified 9/29/2014
-REM Path to MSBuild must be in your PATH environment variable in order for this batch file to work.
+rem Build.bat 
+rem Batch file for building lcmsnet with dmstools and plugins as if using the PNNL internal installer.
+rem Written by Christopher Walters 9/12/2014
+rem Updated 03/02/2017 to auto-define the path to MSBuild.exe if necessary
 
 SETLOCAL
 SET ROOTPATH=%~dp0
 SET CONFIG=/p:Configuration=PNNLRelease /p:Platform=x86
 SET VERB=/verbosity:quiet
 SET LOG=/fileLogger
-SET LOGFILE=%ROOTPATH%lcmsnetBuild.txt
+SET LOGFILE="%ROOTPATH%lcmsnetBuild.txt"
 SET LGPARAMS=/fileloggerparameters:logfile=%LOGFILE%;append=true
-REM SET NOLOGO=/nologo
-SET BOPTS=%VERB% %LOG% %LGPARAMS% 
+rem SET NOLOGO=/nologo
+SET BOPTS=%VERB% %LOG% %LGPARAMS%
+
+rem Determine the path to MSBuild
+where /q msbuild.exe
+IF ERRORLEVEL 1 (
+   echo "Using MSBuild.exe at C:\Program Files (x86)\MSBuild\14.0\Bin\"
+   echo.
+   SET MSBuildExe="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
+) Else (
+   echo "Using MSBuild.exe in your path"
+   echo.
+   Set MSBuildExe=msbuild.exe
+)
 
 if exist %LOGFILE% DEL %LOGFILE%
-REM Build LcmsNetSDK solution
-ECHO Building LcmsNetSDK solution...
-MSBuild %CONFIG% %BOPTS% %ROOTPATH%LcmsNet\SDK\LcmsNetSDKs.sln
-if errorlevel 1 goto :ERROR
-ECHO Done.
 
-REM Build LcmsNetSQLiteTools solution
-ECHO Building LcmsNetSQLiteTools solution...
-MSBUILD %CONFIG% %BOPTS% %ROOTPATH%LcmsNet\SQLiteTools\SQLiteTools.sln
+rem Build LcmsNetSDK solution
+echo Building LcmsNetSDK solution...
+@echo on
+%MSBuildExe% %CONFIG% %BOPTS% "%ROOTPATH%LcmsNet\SDK\LcmsNetSDKs.sln"
+@echo off
 if errorlevel 1 goto :ERROR
-ECHO Done.
+echo Done.
 
-REM Build LcmsNetDMSTools
-ECHO Building LcmsNetDmsTools solution...
-MSBUILD %CONFIG% %BOPTS% %ROOTPATH%LcmsNetDmsTools\LcmsNetDmsTools.sln
+rem Build LcmsNetSQLiteTools solution
+echo Building LcmsNetSQLiteTools solution...
+@echo on
+%MSBuildExe% %CONFIG% %BOPTS% "%ROOTPATH%LcmsNet\SQLiteTools\SQLiteTools.sln"
+@echo off
 if errorlevel 1 goto :ERROR
-ECHO Done.
+echo Done.
 
-REM Build Plugins
-ECHO Building LcmsNetPlugins...
-MSBUILD %CONFIG% %BOPTS% %ROOTPATH%lcmsNetPlugins\lcmsnetPlugins.sln
+rem Build LcmsNetDMSTools
+echo Building LcmsNetDmsTools solution...
+@echo on
+%MSBuildExe% %CONFIG% %BOPTS% "%ROOTPATH%LcmsNetDmsTools\LcmsNetDmsTools.sln"
+@echo off
 if errorlevel 1 goto :ERROR
-ECHO Done.
+echo Done.
 
-REM Build Main solution.
-REM If we're not building in release mode, make sure to copy library files into proper
-REM locations for testing purposes.
+rem Build Plugins
+echo Building LcmsNetPlugins...
+@echo on
+%MSBuildExe% %CONFIG% %BOPTS% "%ROOTPATH%lcmsNetPlugins\lcmsnetPlugins.sln"
+@echo off
+if errorlevel 1 goto :ERROR
+echo Done.
+
+rem Build Main solution.
+rem If we're not building in release mode, make sure to copy library files into proper
+rem locations for testing purposes.
 IF [%1]==[] (
-ECHO Building LcmsNet...
-MSBUILD /p:Configuration=Debug /p:targetPlatform=x86 %BOPTS% %ROOTPATH%LcmsNet\lcms\LCMSNet\LCMSNet.sln 
+   echo Building LcmsNet...
+   @echo on
+   %MSBuildExe% /p:Configuration=Debug /p:targetPlatform=x86 %BOPTS% "%ROOTPATH%LcmsNet\lcms\LCMSNet\LCMSNet.sln"
+   @echo off
 
-ECHO Copying DmsTools to %APPDATA%\LCMSNet\dmsExtensions
-xcopy /y /q %ROOTPATH%LcmsNetDmsTools\LcmsNetDmsTools\bin\x86\PNNLRelease\* %APPDATA%\LCMSNet\dmsExtensions\
-xcopy /y /q %ROOTPATH%LcmsNetDmsTools\LcmsNetDmsTools\prismDMS.config %APPDATA%\LCMSNet\dmsExtensions\
-if errorlevel 1 goto :ERROR
-ECHO Done.
+   echo Copying DmsTools to %APPDATA%\LCMSNet\dmsExtensions
+   xcopy /y /q /d "%ROOTPATH%LcmsNetDmsTools\LcmsNetDmsTools\bin\x86\PNNLRelease\*" %APPDATA%\LCMSNet\dmsExtensions\
+   xcopy /y /q /d "%ROOTPATH%LcmsNetDmsTools\LcmsNetDmsTools\prismDMS.config" %APPDATA%\LCMSNet\dmsExtensions\
+   if errorlevel 1 goto :ERROR
+   echo Done.
 
-ECHO Copying Plugins to %ROOTPATH%LcmsNet\lcms\LCMSNet\LCMSNetProg\bin\x86\debug\plugins
-xcopy /y /q %ROOTPATH%pluginDLLs\*.dll %ROOTPATH%LcmsNet\lcms\LCMSNet\LCMSNetProg\bin\x86\debug\plugins\
-xcopy /y /q %ROOTPATH%LcmsNet\lib\FluidicsPack.dll %ROOTPATH%LcmsNet\lcms\LCMSNet\LCMSNetProg\bin\x86\debug\plugins
-if errorlevel 1 goto :ERROR
-ECHO Done.
+   echo Copying Plugins to "%ROOTPATH%LcmsNet\lcms\LCMSNet\LCMSNetProg\bin\x86\debug\plugins"
+   xcopy /y /q /d "%ROOTPATH%pluginDLLs\*.dll" "%ROOTPATH%LcmsNet\lcms\LCMSNet\LCMSNetProg\bin\x86\debug\plugins\"
+   xcopy /y /q /d "%ROOTPATH%LcmsNet\lib\FluidicsPack.dll" "%ROOTPATH%LcmsNet\lcms\LCMSNet\LCMSNetProg\bin\x86\debug\plugins"
+   if errorlevel 1 goto :ERROR
+   echo Done.
 
-ECHO Copying Core Validator to %APPDATA%\LCMSNet\SampleValidators
-xcopy /y /q %ROOTPATH%\LcmsNet\SDK\CoreSampleValidator\bin\x86\PNNLRelease\*.dll %APPDATA%\LCMSNet\SampleValidators\
-ECHO Done.
+   echo Copying Core Validator to %APPDATA%\LCMSNet\SampleValidators
+   xcopy /y /q /d "%ROOTPATH%\LcmsNet\SDK\CoreSampleValidator\bin\x86\PNNLRelease\*.dll" %APPDATA%\LCMSNet\SampleValidators\
+   echo Done.
 
-ECHO Copying PAL Validator to %APPDATA%\LCMSNet\SampleValidators
-xcopy /y /q %ROOTPATH%\LcmsNetPlugins\PalValidator\bin\x86\debug\*.dll %APPDATA%\LCMSNet\SampleValidators\
-ECHO Done.
-goto :COMPLETE)
+   echo Copying PAL Validator to %APPDATA%\LCMSNet\SampleValidators
+   xcopy /y /q /d "%ROOTPATH%\LcmsNetPlugins\PalValidator\bin\x86\debug\*.dll" %APPDATA%\LCMSNet\SampleValidators\
+   echo Done.
+   goto :COMPLETE
+)
 
-
-REM If we're building in release mode, we don't need to do all the copying of files, since the
-REM installer script already knows where to look for the library files, and knows where to put
-REM them during install.
+rem If we're building in release mode, we don't need to do all the copying of files, since the
+rem installer script already knows where to look for the library files, and knows where to put
+rem them during install.
 IF %1==release (
-ECHO Building LcmsNet in release mode...
-MSBUILD %CONFIG% %BOPTS% %ROOTPATH%LcmsNet\lcms\LCMSNet\LCMSNet.sln
-if errorlevel 1 goto :ERROR
-ECHO Done.
+   echo Building LcmsNet in release mode...
+   @echo on
+   %MSBuildExe% %CONFIG% %BOPTS% "%ROOTPATH%LcmsNet\lcms\LCMSNet\LCMSNet.sln"
+   @echo off
+   if errorlevel 1 goto :ERROR
+   echo Done.
 
-ECHO Building LogViewer...
-MSBUILD %CONFIG% %BOPTS% %ROOTPATH%\ExternalApplications\LogViewer\LogViewer.sln
-ECHO Done.
-goto :COMPLETE)
+   echo Building LogViewer...
+   @echo on
+   %MSBuildExe% %CONFIG% %BOPTS% "%ROOTPATH%\ExternalApplications\LogViewer\LogViewer.sln"
+   @echo off
+   echo Done.
+   goto :COMPLETE
+)
 
 :ERROR
-ECHO Error occured during build. See lcmsnetBuild.txt for more details.
+echo Error occured during build. See lcmsnetBuild.txt for more details.
 goto :END
 
 :COMPLETE
-ECHO Cleaning up...
+echo Cleaning up...
 DEL %LOGFILE%
-ECHO BUILD COMPLETE.
+
+echo BUILD COMPLETE.
 
 :END
