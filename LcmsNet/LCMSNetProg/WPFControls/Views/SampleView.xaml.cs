016158964ff31f1031ddbee78938fa8a72f67955
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2017-05-09 13:21:06 -0700

    Remove unused "using" directives in the xaml code-behinds

--- LcmsNet/LCMSNetProg/WPFControls/Views/SampleView.xaml.cs
+++ LcmsNet/LCMSNetProg/WPFControls/Views/SampleView.xaml.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Media;
 using LcmsNet.WPFControls.ViewModels;
 
 namespace LcmsNet.WPFControls.Views
