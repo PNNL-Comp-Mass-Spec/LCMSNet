d363199fd37627917afec6bb2464d959551cb027
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2017-06-17 18:04:38 -0700

    WPF Versions of the controls in the VICI plugin

---
+++ lcmsnetPlugins/VICI/Devices/Valves/ValveVICI2PosView.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LcmsNet.Devices.Valves
+{
+    /// <summary>
+    /// Interaction logic for ValveVICI2PosView.xaml
+    /// </summary>
+    public partial class ValveVICI2PosView : UserControl
+    {
+        public ValveVICI2PosView()
+        {
+            InitializeComponent();
+        }
+
+        public SerialPort SerialPortBinding
+        {
+            get { return (SerialPort)GetValue(SerialPortBindingProperty); }
+            set { SetValue(SerialPortBindingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SerialPortBinding.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SerialPortBindingProperty =
+            DependencyProperty.Register("SerialPortBinding", typeof(SerialPort), typeof(ValveVICI2PosView), new PropertyMetadata(null, PropertyChangedCallback));
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            if (!(dependencyObject is ValveVICI2PosView v))
+            {
+                return;
+            }
+            v.WinFormsPropertyGrid.SelectedObject = dependencyPropertyChangedEventArgs.NewValue;
+        }
+
+        private void PropertyGrid_OnPropertyValueChanged(object s, System.Windows.Forms.PropertyValueChangedEventArgs e)
+        {
+            if (this.DataContext is ValveVICI2PosViewModel v)
+            {
+                v.OnSaveRequired();
+            }
+        }
+
+        private void ValveVICI2PosView_OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue is ValveVICIViewModelBase vm)
+            {
+                var binding = new Binding("ComPort");
+                binding.Source = e.NewValue;
+                this.SetBinding(SerialPortBindingProperty, binding);
+            }
+        }
+    }
+}
---
+++ lcmsnetPlugins/VICI/Devices/Valves/ValveVICIViewModelBase.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Reactive;
+using System.Windows;
+using System.Windows.Controls;
+using LcmsNetDataClasses.Devices;
+using LcmsNetDataClasses.Logging;
+using LcmsNetSDK;
+
+namespace LcmsNet.Devices.Valves
+{
+    public abstract class ValveVICIViewModelBase : BaseDeviceControlViewModel, IDeviceControlWpf
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ValveVICIViewModelBase()
+        {
+
+            using (ValveIdComboBoxOptions.SuppressChangeNotifications())
+            {
+                ValveIdComboBoxOptions.AddRange(new [] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ' });
+            }
+
+            SetupCommands();
+
+            this.PropertyChanged += OnPropertyChanged;
+        }
+
+        protected abstract void RegisterDevice(IDevice device);
+
+        #endregion
+
+        #region Members
+
+        //public ValveEventListener m_valveEventListener;
+
+        private readonly ReactiveUI.ReactiveList<char> valveIdComboBoxOptions = new ReactiveUI.ReactiveList<char>();
+        private char selectedValveId = ' ';
+        private char currentValveId = ' ';
+        private string currentValvePosition = "";
+        private string valveVersionInfo = "";
+        private bool valveControlTabSelected = false;
+        private SerialPort comPort;
+
+        #endregion
+
+        #region Properties
+
+        public ReactiveUI.ReactiveList<char> ValveIdComboBoxOptions => valveIdComboBoxOptions;
+
+        public char SelectedValveId
+        {
+            get { return selectedValveId; }
+            set { this.RaiseAndSetIfChanged(ref selectedValveId, value); }
+        }
+
+        public char CurrentValveId
+        {
+            get { return currentValveId; }
+            protected set { this.RaiseAndSetIfChanged(ref currentValveId, value); }
+        }
+
+        public string CurrentValvePosition
+        {
+            get { return currentValvePosition; }
+            protected set { this.RaiseAndSetIfChanged(ref currentValvePosition, value); }
+        }
+
+        public string ValveVersionInfo
+        {
+            get { return valveVersionInfo; }
+            protected set { this.RaiseAndSetIfChanged(ref valveVersionInfo, value); }
+        }
+
+        public bool ValveControlTabSelected
+        {
+            get { return valveControlTabSelected; }
+            set { this.RaiseAndSetIfChanged(ref valveControlTabSelected, value); }
+        }
+
+        /// <summary>
+        /// The serial port used for communicating with the Valve
+        /// </summary>
+        public SerialPort ComPort
+        {
+            get { return comPort; }
+            protected set { this.RaiseAndSetIfChanged(ref comPort, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the associated device.
+        /// </summary>
+        public abstract IDevice Device { get; set; }
+
+        public abstract UserControl GetDefaultView();
+
+        #endregion
+
+        #region Commands
+
+        public ReactiveUI.ReactiveCommand<Unit, Unit> ClearValveIdCommand { get; private set; }
+        public ReactiveUI.ReactiveCommand<Unit, Unit> RefreshValveIdCommand { get; private set; }
+        public ReactiveUI.ReactiveCommand<Unit, Unit> RefreshValvePositionCommand { get; private set; }
+        public ReactiveUI.ReactiveCommand<Unit, Unit> RefreshValveVersionInfoCommand { get; private set; }
+        public ReactiveUI.ReactiveCommand<Unit, Unit> OpenPortCommand { get; private set; }
+        public ReactiveUI.ReactiveCommand<Unit, Unit> ClosePortCommand { get; private set; }
+        public ReactiveUI.ReactiveCommand<Unit, Unit> InitializeDeviceCommand { get; private set; }
+
+        private void SetupCommands()
+        {
+            ClearValveIdCommand = ReactiveUI.ReactiveCommand.Create(() => ClearValveId());
+            RefreshValveIdCommand = ReactiveUI.ReactiveCommand.Create(() => RefreshValveId());
+            RefreshValvePositionCommand = ReactiveUI.ReactiveCommand.Create(() => RefreshValvePosition());
+            RefreshValveVersionInfoCommand = ReactiveUI.ReactiveCommand.Create(() => RefreshValveVersion());
+            OpenPortCommand = ReactiveUI.ReactiveCommand.Create(() => OpenPort());
+            ClosePortCommand = ReactiveUI.ReactiveCommand.Create(() => ClosePort());
+            InitializeDeviceCommand = ReactiveUI.ReactiveCommand.Create(() => InitializeDevice());
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            if (propertyChangedEventArgs.PropertyName.Equals(nameof(SelectedValveId)))
+            {
+                SelectedValveIdUpdated();
+            }
+            if (propertyChangedEventArgs.PropertyName.Equals(nameof(ValveControlTabSelected)) && ValveControlTabSelected)
+            {
+                ValveControlSelected();
+            }
+        }
+
+        public virtual void Valve_DeviceSaveRequired(object sender, EventArgs e)
+        {
+            OnSaveRequired();
+        }
+
+        /// <summary>
+        /// Displays an error message
+        /// </summary>
+        /// <param name="message">The message to display</param>
+        protected void ShowError(string message)
+        {
+            classApplicationLogger.LogError(classApplicationLogger.CONST_STATUS_LEVEL_CRITICAL, message);
+        }
+
+        /// <summary>
+        /// Displays an error message
+        /// </summary>
+        /// <param name="message">The message to display</param>
+        /// <param name="ex">The exception to describe</param>
+        protected void ShowError(string message, Exception ex)
+        {
+            MessageBox.Show(message + "\r\n" + ex.Message, Device.Name);
+        }
+
+        protected abstract void ValveControlSelected();
+
+        protected abstract void RefreshValvePosition();
+
+        protected abstract void RefreshValveVersion();
+
+        protected abstract void OpenPort();
+
+        protected abstract void ClosePort();
+
+        protected abstract void InitializeDevice();
+
+        protected abstract void SelectedValveIdUpdated();
+
+        protected abstract void RefreshValveId();
+
+        protected abstract void ClearValveId();
+
+        #endregion
+    }
+}
