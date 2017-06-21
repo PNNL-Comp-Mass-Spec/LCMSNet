92c1b248562522af3948986829483f0f2f22944b
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2017-06-20 18:55:53 -0700

    WPF version of the Default device view model and view

---
+++ LcmsNet/LCMSNetProg/Devices/DefaultUserDeviceViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using LcmsNetDataClasses.Devices;
+
+namespace LcmsNet.Devices
+{
+    public class DefaultUserDeviceViewModel : IDeviceControlWpf
+    {
+        public DefaultUserDeviceViewModel()
+        {
+        }
+
+        #region IDeviceControl Members
+
+        public IDevice Device { get; set; }
+        public string Name { get; set; }
+        public UserControl GetDefaultView()
+        {
+            return new DefaultUserDeviceView();
+        }
+
+        /// <summary>
+        /// An event that indicates the name of the column has changed.
+        /// </summary>
+        /// <remarks>This event is required by IDeviceControl but this class does not use it</remarks>
+        public event DelegateNameChanged NameChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public bool Running
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// An event that indicates the control needs to be saved
+        /// </summary>
+        /// <remarks>This event is required by IDeviceControl but this class does not use it</remarks>
+        public event DelegateSaveRequired SaveRequired
+        {
+            add { }
+            remove { }
+        }
+
+        #endregion
+    }
+}
