b2a10ab561186bc9d118b844638a1fc39cf8c1f1
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2017-10-25 14:16:02 -0700

    Update a plugin test to WPF and AnyCPU

---
+++ lcmsnetPlugins/ASIpumpTest/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace ASIpumpTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Event handler that is triggered on application start up.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = new AsiTestWindow();
+            var mainWindowVm = new AsiTestViewModel();
+            mainWindow.DataContext = mainWindowVm;
+            mainWindow.Show();
+        }
+    }
+}
---
+++ lcmsnetPlugins/ASIpumpTest/AsiTestViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASIpump;
+using ReactiveUI;
+
+namespace ASIpumpTest
+{
+    public class AsiTestViewModel : ReactiveObject
+    {
+        public AsiTestViewModel()
+        {
+            asiUIVM = new AsiUIViewModel();
+            var pump = new ASIpump.AsiPump();
+            pump.PortName = "COM3";
+
+            asiUIVM.Pump = pump;
+        }
+
+        private readonly AsiUIViewModel asiUIVM;
+
+        public AsiUIViewModel AsiUIVM => asiUIVM;
+    }
+}
