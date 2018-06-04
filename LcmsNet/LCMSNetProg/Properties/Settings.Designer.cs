ae92d20250e99c06545337a726908430c992b535
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2018-06-04 12:26:13 -0700

    Bump the minimum DotNET Framework to 4.6.2

--- LcmsNet/LCMSNetProg/Properties/Settings.Designer.cs
+++ LcmsNet/LCMSNetProg/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace LcmsNet.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -317,14 +317,11 @@ namespace LcmsNet.Properties {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        public string ColumnDisabledSpecial
-        {
-            get
-            {
+        public string ColumnDisabledSpecial {
+            get {
                 return ((string)(this["ColumnDisabledSpecial"]));
             }
-            set
-            {
+            set {
                 this["ColumnDisabledSpecial"] = value;
             }
         }
