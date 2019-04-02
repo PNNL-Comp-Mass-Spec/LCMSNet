c080b575ead32a9afa6ea3327a73ab41f46c8619
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2019-04-01 17:56:29 -0700

    Add a WPF exe for testing communication/controlling an agilent pump outside of the LCMSNet GUI

---
+++ lcmsnetPlugins/AgilentPumpExe/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace AgilentPumpExe
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+
+            var mainVm = new MainWindowViewModel();
+            MainWindow = new MainWindow() { DataContext = mainVm };
+
+            try
+            {
+                MainWindow.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Unhandled Exception, Closing", MessageBoxButton.OK, MessageBoxImage.Error);
+                MainWindow.Close();
+            }
+        }
+
+        private void CurrentDomain_FirstChanceException(object sender, FirstChanceExceptionEventArgs e)
+        {
+            // TODO: This is the best place to notify about exceptions, but catches others we don't want to worry about. It also doesn't let us say that it has been handled.
+        }
+
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            var ex = e.Exception;
+            if (e.Exception.GetType() == typeof(ReactiveUI.UnhandledErrorException) && e.Exception.InnerException != null)
+            {
+                ex = e.Exception.InnerException;
+            }
+            MessageBox.Show(ex.ToString(), "Unhandled Exception Caught", MessageBoxButton.OK, MessageBoxImage.Error);
+            Shutdown();
+        }
+    }
+}
---
+++ lcmsnetPlugins/AgilentPumpExe/MainWindowViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using LcmsNetData.Logging;
+using LcmsNetPlugins.Agilent.Pumps;
+using ReactiveUI;
+
+namespace AgilentPumpExe
+{
+    public class MainWindowViewModel : ReactiveObject, IDisposable
+    {
+        private string status = "uninitialized";
+        private bool initialized = false;
+        private ReactiveList<string> statusHistory = new ReactiveList<string>();
+
+        public AgilentPumpViewModel AgilentPumpVm { get; } = new AgilentPumpViewModel();
+        public AgilentPump AgilentPump { get; } = new AgilentPump(true);
+        public ReactiveCommand<Unit, Unit> InitializePumpCommand { get; }
+        public ReactiveCommand<Unit, Unit> ClearStatusHistoryCommand { get; }
+
+        public string Status
+        {
+            get => status;
+            set => this.RaiseAndSetIfChanged(ref status, value);
+        }
+
+        public bool Initialized
+        {
+            get => initialized;
+            set => this.RaiseAndSetIfChanged(ref initialized, value);
+        }
+
+        public IReadOnlyReactiveList<string> StatusHistory => statusHistory;
+
+        public MainWindowViewModel()
+        {
+            AgilentPump.DisableMonitoring();
+            AgilentPumpVm.Device = AgilentPump;
+
+            InitializePumpCommand = ReactiveCommand.Create(InitializePump, this.WhenAnyValue(x => x.Initialized).Select(x => !x));
+            ApplicationLogger.Error += ApplicationLoggerOnError;
+            ApplicationLogger.Message += ApplicationLoggerOnMessage;
+        }
+
+        private void ApplicationLoggerOnError(int errorLevel, ErrorLoggerArgs args)
+        {
+            // TODO: Add exception unwind
+            RxApp.MainThreadScheduler.Schedule(() => AddStatusHistoryItem("ERROR", args.Message));
+        }
+
+        private void ApplicationLoggerOnMessage(int messageLevel, MessageLoggerArgs args)
+        {
+            RxApp.MainThreadScheduler.Schedule(() => AddStatusHistoryItem("INFO", args.Message));
+        }
+
+        private void AddStatusHistoryItem(string type, string message)
+        {
+            statusHistory.Add($"{type}:   {message}");
+        }
+
+        public void InitializePump()
+        {
+            var errors = "";
+            AgilentPump.Initialize(ref errors);
+            Status = errors;
+            if (string.IsNullOrWhiteSpace(errors))
+            {
+                initialized = true;
+            }
+        }
+
+        public void ClearStatusHistory()
+        {
+            using (statusHistory.SuppressChangeNotifications())
+            {
+                statusHistory.Clear();
+            }
+        }
+
+        public void Dispose()
+        {
+            AgilentPumpVm?.Dispose();
+            AgilentPump?.Dispose();
+            InitializePumpCommand?.Dispose();
+            ClearStatusHistoryCommand?.Dispose();
+        }
+    }
+}
